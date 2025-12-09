d724495d4c06b2ce236dcfde6af62e71a1e04ecb
spiros dimopulos <spiros@wantedpixel.com>
2025-12-09 11:36:27 +0200

    DRYD-1904: added materialTechniqueDescription field;

--- src/plugins/recordTypes/collectionobject/fields.js
+++ src/plugins/recordTypes/collectionobject/fields.js
@@ -712,6 +712,19 @@ export default (configContext) => {
             },
           },
         },
+        materialTechniqueDescription: {
+          [config]: {
+            messages: defineMessages({
+              name: {
+                id: 'field.collectionobjects_anthro.materialTechniqueDescription.name',
+                defaultMessage: 'Material/technique description',
+              },
+            }),
+            view: {
+              type: TextInput,
+            },
+          },
+        },
         // FIXME: Locality was added for DRYD-400, but I didn't realize locality was already
         // included by the naturalhistory extension, so the locality fields are duplicated in both
         // collectionobjects_naturalhistory_extension and collectionobjects_anthro.
