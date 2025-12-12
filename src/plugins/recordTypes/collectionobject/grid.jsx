7f06447ac394b8b8b51cdac24a69a77e4144cc86
Michael Ritter <mikejritter@users.noreply.github.com>
2025-12-12 16:18:46 -0700

    DRYD-1981: Add NAGPRA-eligible tag formatter (#37)

---
+++ src/plugins/recordTypes/collectionobject/grid.jsx
@@ -0,0 +1,34 @@
+export default (configContext) => {
+  const {
+    React,
+    Immutable,
+    FormattedMessage,
+  } = configContext.lib;
+
+  return {
+    tags: {
+      formatter: (data) => {
+        let nagpraEligible;
+        const nagpraCategoriesList = data.get('nagpraCategories');
+        if (nagpraCategoriesList) {
+          const nagpraCategories = nagpraCategoriesList.get('nagpraCategory');
+          if (Immutable.List.isList(nagpraCategories)) {
+            nagpraEligible = nagpraCategories.filter((category) => !!category).size > 1;
+          } else {
+            nagpraEligible = nagpraCategories && nagpraCategories.length > 0;
+          }
+        }
+
+        const eligible = (
+          <FormattedMessage
+            id="grid.collectionobject.nagpraEligible"
+            description="The text for NAGPRA eligible results the search grid view"
+            defaultMessage="NAGPRA-eligible"
+          />
+        );
+
+        return nagpraEligible ? <p>{eligible}</p> : null;
+      },
+    },
+  };
+};
