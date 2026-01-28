62bb33d20f162da97018d1fe0a5adfae90c7c131
Michael Ritter <mikejritter@users.noreply.github.com>
2026-01-28 11:36:13 -0700

    DRYD-1938: Detail List QA Updates (#39)
    * Incorporates content concept changes from cspace-ui.js
    * Change prefix from NAGPRA Determinations to NAGPRA DETERMINATION

--- src/plugins/recordTypes/collectionobject/detailList.jsx
+++ src/plugins/recordTypes/collectionobject/detailList.jsx
@@ -1,8 +1,24 @@
+import { defineMessages } from 'react-intl';
+
+const messages = defineMessages({
+  concepts: {
+    id: 'detailList.tags.collectionobject.concepts',
+    description: 'The prefix for content concept tags in the search detail view',
+    defaultMessage: `{count, plural,
+              one {CONCEPT TAG: }
+              other {CONCEPT TAGS: }}`,
+  },
+  determination: {
+    id: 'detailList.tags.collectionobject.determinations',
+    description: 'The prefix for NAGPRA determination tags in the search detail view',
+    defaultMessage: 'NAGPRA DETERMINATION: ',
+  },
+});
+
 export default (configContext) => {
   const {
     React,
     Immutable,
-    FormattedMessage,
   } = configContext.lib;
 
   const {
@@ -11,30 +27,26 @@ export default (configContext) => {
 
   return {
     tags: {
-      formatter: (data, separator = ', ') => {
+      formatter: (data, { intl, separator = ', ' } = {}) => {
         let concepts;
         const contentConcepts = data.get('contentConcepts');
         if (contentConcepts) {
+          let count = 0;
           let conceptTags;
-          const conceptPrefix = (
-            <FormattedMessage
-              id="detailList.collectionobject.concepts"
-              description="The prefix for content concept tags in the search detail view"
-              defaultMessage="Content Concepts: "
-            />
-          );
           const contentConcept = contentConcepts.get('contentConcept');
           if (Immutable.List.isList(contentConcept)) {
             conceptTags = contentConcept.filter((concept) => !!concept)
               .map((concept) => formatRefName(concept))
               .join(separator);
+            count = contentConcept.size;
           } else {
             conceptTags = formatRefName(contentConcept);
+            count = 1;
           }
 
           concepts = (
             <p>
-              {conceptPrefix}
+              <span>{intl.formatMessage(messages.concepts, { count })}</span>
               {conceptTags}
             </p>
           );
@@ -44,14 +56,6 @@ export default (configContext) => {
         const nagpraCategories = data.get('nagpraCategories');
         if (nagpraCategories) {
           let nagpraDeterminations;
-          const nagpraPrefix = (
-            <FormattedMessage
-              id="detailList.collectionobject.determinations"
-              description="The prefix for NAGPRA determination tags in the search detail view"
-              defaultMessage="NAGPRA Determinations: "
-            />
-          );
-
           const nagpraCategory = nagpraCategories.get('nagpraCategory');
           if (Immutable.List.isList(nagpraCategory)) {
             nagpraDeterminations = nagpraCategory.filter((category) => !!category)
@@ -62,7 +66,7 @@ export default (configContext) => {
           }
           determinations = (
             <p>
-              {nagpraPrefix}
+              <span>{intl.formatMessage(messages.determination)}</span>
               {nagpraDeterminations}
             </p>
           );
